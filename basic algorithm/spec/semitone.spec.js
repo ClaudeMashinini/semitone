1c37433602558b2a8f629157509473fee5ad3c0e
Claude Mashinini <dumi.claude@gmail.com>
2020-03-09 17:34:28 +0200

    first part and tests done

---
+++ basic algorithm/spec/semitone.spec.js
@@ -0,0 +1,28 @@
+const JamBuddy = require("../app");
+let buddy = new JamBuddy();
+
+describe("checking for the presence of a full note circle", function() {
+  it("should have 12 notes, in chronological order", function() {
+    expect(buddy.notes[0]).toBe("A");
+    expect(buddy.notes[1]).toBe("A#");
+    expect(buddy.notes[2]).toBe("B");
+    expect(buddy.notes[3]).toBe("C");
+    expect(buddy.notes[4]).toBe("C#");
+    expect(buddy.notes[5]).toBe("D");
+    expect(buddy.notes[6]).toBe("D#");
+    expect(buddy.notes[7]).toBe("E");
+    expect(buddy.notes[8]).toBe("F");
+    expect(buddy.notes[9]).toBe("F#");
+    expect(buddy.notes[10]).toBe("G");
+    expect(buddy.notes[11]).toBe("G#");
+  });
+});
+
+describe("basic functionality", function() {
+  it("should be able to randomly pick two notes from the notes' array", function() {
+    expect(buddy.selectNotes().length).toBe(2);
+  });
+  it("should be able to determine whether the predicted number of seminotes is true or not", function() {
+    expect(buddy.checkAnswer(3)).toBe(true || false);
+  });
+});

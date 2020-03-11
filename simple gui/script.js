beaa0013cc2c344749cce42cea2532b7387fe81a
Claude Mashinini <dumi.claude@gmail.com>
2020-03-11 10:48:25 +0200

    gui index page update

---
+++ simple gui/script.js
@@ -0,0 +1,36 @@
+module.exports = class JamBuddy {
+  constructor() {
+    this.notes = [
+      "A",
+      "A#",
+      "B",
+      "C",
+      "C#",
+      "D",
+      "D#",
+      "E",
+      "F",
+      "F#",
+      "G",
+      "G#"
+    ];
+  }
+  selectNotes() {
+    let output, firstNote, secondNote;
+    firstNote = this.notes[Math.floor(Math.random() * this.notes.length)];
+    secondNote = this.notes[Math.floor(Math.random() * (12 - 1) + 1) - 1];
+
+    output = [firstNote, secondNote];
+    this.final = output.sort();
+    return this.final;
+  }
+  checkAnswer(prod) {
+    if (
+      this.notes.indexOf(this.final[1]) - this.notes.indexOf(this.final[0]) ==
+      prod
+    ) {
+      return true;
+    }
+    return false;
+  }
+};
